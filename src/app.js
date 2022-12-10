2723e6ffc17267df4ad3bc7a7ed3a63d7ef50108
yaaHosein <layali.14.peshawar@hotmail.com>
2022-12-10 15:40:26 +0330

    making todo list

---
+++ src/app.js
@@ -0,0 +1,17 @@
+export class App {
+  constructor() {
+    this.toDos = [];
+    this.toDoDescription = "";
+  }
+
+  addToDo() {
+    this.toDos.push({ description: this.toDoDescription });
+    this.toDoDescription = "";
+  }
+
+  
+  removeToDo(toDo) {
+    let index = this.toDos.indexOf(toDo);
+    this.toDos.splice(index);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { Aurelia } from "aurelia-framework";
+import environment from "../config/environment.json";
+import { PLATFORM } from "aurelia-pal";
+import "bootstrap/dist/css/bootstrap.css";
+
+export function configure(aurelia: Aurelia): void {
+  aurelia.use
+    .standardConfiguration()
+    .feature(PLATFORM.moduleName("resources/index"));
+
+    
+  aurelia.use.developmentLogging(environment.debug ? "debug" : "warn");
+
+  if (environment.testing) {
+    aurelia.use.plugin(PLATFORM.moduleName("aurelia-testing"));
+  }
+
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName("app")));
+}
